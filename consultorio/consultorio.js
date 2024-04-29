6d98516f06f989b5b5d0b801346374a7a3282d1a
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-04-28 22:56:56 -0300

    reconexion automatica al servidor websocket

--- consultorio/consultorio.js
+++ consultorio/consultorio.js
@@ -1,4 +1,4 @@
-// Establecer conexión WebSocket con el servidor
+/* // Establecer conexión WebSocket con el servidor
 const ws = new WebSocket(
   "wss://llamadorpuntocardio-d37f8860669d.herokuapp.com"
 );
@@ -54,4 +54,73 @@ if (btnEnviar) {
   });
 } else {
   console.error("No se encontró el botón de enviar.");
+} */
+
+// Función para establecer la conexión WebSocket con el servidor y manejar la reconexión automática
+function connectWebSocket() {
+  const ws = new WebSocket(
+    "wss://llamadorpuntocardio-d37f8860669d.herokuapp.com"
+  );
+
+  // Manejar eventos al abrir la conexión WebSocket
+  ws.addEventListener("open", function (event) {
+    console.log("Conexión WebSocket establecida.");
+  });
+
+  // Manejar eventos de error en la conexión WebSocket
+  ws.addEventListener("error", function (error) {
+    console.error("Error en la conexión WebSocket:", error);
+  });
+
+  // Manejar eventos de cierre de conexión
+  ws.addEventListener("close", function (event) {
+    console.log("Conexión WebSocket cerrada. Intentando reconectar...");
+    setTimeout(connectWebSocket, 3000); // Intentar reconectar después de 3 segundos
+  });
+
+  // Manejar el envío de datos del formulario al hacer clic en el botón "Enviar"
+  const btnEnviar = document.querySelector(".btn-enviar");
+  if (btnEnviar) {
+    btnEnviar.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Obtener el valor del consultorio seleccionado
+      const consultorioSeleccionado = document
+        .querySelector('input[name="consultorio"]:checked')
+        .value.toUpperCase();
+
+      // Obtener los valores de los campos de texto y convertirlos a mayúsculas
+      let nombreActual = document
+        .getElementById("nombreActual")
+        .value.toUpperCase();
+      let nombreSiguiente = document
+        .getElementById("nombreSiguiente")
+        .value.toUpperCase();
+
+      // Verificar que los campos no estén vacíos
+      if (nombreActual.trim() === "" || nombreSiguiente.trim() === "") {
+        console.error("Los campos no pueden estar vacíos.");
+        return;
+      }
+
+      // Crear un objeto con los datos a enviar al servidor, incluyendo el consultorio seleccionado
+      const data = {
+        nombreActual,
+        nombreSiguiente,
+        consultorio: consultorioSeleccionado,
+      };
+
+      // Enviar los datos al servidor en formato JSON
+      ws.send(JSON.stringify(data));
+
+      // Actualizar los campos de texto después de enviar los datos
+      document.getElementById("nombreActual").value = nombreSiguiente;
+      document.getElementById("nombreSiguiente").value = "";
+    });
+  } else {
+    console.error("No se encontró el botón de enviar.");
+  }
 }
+
+// Llamar a la función connectWebSocket para iniciar la conexión WebSocket al cargar la página
+connectWebSocket();
--- salaDeEspera/salaDeEspera.js
+++ salaDeEspera/salaDeEspera.js
@@ -1,4 +1,4 @@
-// Establecer conexión WebSocket con el servidor
+/* // Establecer conexión WebSocket con el servidor
 const ws = new WebSocket(
   "wss://llamadorpuntocardio-d37f8860669d.herokuapp.com"
 );
@@ -88,4 +88,111 @@ function reproducirAlertaSonido() {
 function mostrarConsultorio(consultorio) {
   const consultorioElement = document.getElementById("nombreConsultorio");
   consultorioElement.textContent = consultorio;
+} */
+
+// Función para establecer la conexión WebSocket con el servidor y manejar la reconexión automática
+function connectWebSocket() {
+  const ws = new WebSocket(
+    "wss://llamadorpuntocardio-d37f8860669d.herokuapp.com"
+  );
+
+  // Manejar eventos al abrir la conexión WebSocket
+  ws.addEventListener("open", function (event) {
+    console.log("Conexión WebSocket establecida.");
+  });
+
+  // Manejar eventos de error en la conexión WebSocket
+  ws.addEventListener("error", function (error) {
+    console.error("Error en la conexión WebSocket:", error);
+  });
+
+  // Manejar eventos de cierre de conexión
+  ws.addEventListener("close", function (event) {
+    console.log("Conexión WebSocket cerrada. Intentando reconectar...");
+    setTimeout(connectWebSocket, 3000); // Intentar reconectar después de 3 segundos
+  });
+
+  // Manejar mensajes recibidos del servidor
+  ws.addEventListener("message", function (event) {
+    console.log("Mensaje recibido del servidor:", event.data);
+
+    // Verificar si event.data es un Blob
+    if (event.data instanceof Blob) {
+      // Crear un FileReader para leer el contenido del Blob
+      const reader = new FileReader();
+      reader.onload = function () {
+        const data = JSON.parse(reader.result);
+        actualizarPacientes(data);
+        reproducirVoz(
+          data.nombreActual,
+          data.nombreSiguiente,
+          data.consultorio
+        );
+        mostrarConsultorio(data.consultorio); // Llamar a la función mostrarConsultorio
+        reproducirAlertaSonido(); // Reproducir alerta de sonido
+      };
+      reader.onerror = function (error) {
+        console.error("Error al leer el contenido del Blob:", error);
+      };
+
+      // Leer el contenido del Blob
+      reader.readAsText(event.data);
+    } else {
+      // Si event.data no es un Blob, asumimos que ya es texto JSON
+      const data = JSON.parse(event.data);
+      actualizarPacientes(data);
+      reproducirVoz(data.nombreActual, data.nombreSiguiente, data.consultorio);
+      mostrarConsultorio(data.consultorio); // Llamar a la función mostrarConsultorio
+      reproducirAlertaSonido(); // Reproducir alerta de sonido
+    }
+  });
+
+  // Función para reproducir el sonido de alerta
+  function reproducirAlertaSonido() {
+    const alertaSonido = document.getElementById("alertaSonido");
+    alertaSonido.play();
+  }
+
+  // Función para actualizar los nombres de los pacientes en la vista
+  function actualizarPacientes(data) {
+    console.log("Datos recibidos:", data);
+
+    const nombrePacienteActual = document.getElementById("nombrePaciente");
+    const nombreSiguientePaciente = document.getElementById("nombreSiguiente");
+
+    nombrePacienteActual.textContent = data.nombreActual;
+    nombreSiguientePaciente.textContent = data.nombreSiguiente;
+  }
+
+  // Función para reproducir la voz anunciando el paciente actual y siguiente en fila
+  function reproducirVoz(nombreActual, nombreSiguiente, consultorio) {
+    const textoVoz = `Ingrese al ${consultorio} ${nombreActual}. siguiente paciente ${nombreSiguiente}.`;
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(textoVoz);
+
+    // Reproducir la voz después de 1 segundo
+    setTimeout(function () {
+      synth.speak(utterance);
+    }, 1500);
+  }
+
+  // Función para reproducir el sonido de alerta al cargar la página
+  window.onload = function () {
+    reproducirAlertaSonido();
+  };
+
+  // Función para reproducir el sonido de alerta
+  function reproducirAlertaSonido() {
+    const alertaSonido = document.getElementById("alertaSonido");
+    alertaSonido.play();
+  }
+
+  // Función para mostrar el consultorio en la vista
+  function mostrarConsultorio(consultorio) {
+    const consultorioElement = document.getElementById("nombreConsultorio");
+    consultorioElement.textContent = consultorio;
+  }
 }
+
+// Llamar a la función connectWebSocket para iniciar la conexión WebSocket al cargar la página
+connectWebSocket();
